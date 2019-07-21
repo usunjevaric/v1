8e5bff8a0a235b220b37ffba2ff31f1630c62096
Sunjevaric <usunjevaric@gmail.com>
2019-07-21 15:24:45 +0200

    add rsponsive navigation, dropdown and home

---
+++ js/main.js
@@ -0,0 +1,44 @@
+window.addEventListener('scroll', changeNavigation);
+
+function changeNavigation() {
+  if (window.pageYOffset > 200) {
+    document.getElementById('nav').classList.remove('navigation');
+    document.getElementById('nav').classList.add('navigation-scrolled');
+  } else {
+    document.getElementById('nav').classList.add('navigation');
+    document.getElementById('nav').classList.remove('navigation-scrolled');
+  }
+}
+
+//dropdown menu on small screens
+const menuBtn = document.querySelector('.collapse-btn');
+const navMenu = document.querySelector('.nav-list');
+
+let showMenu = false;
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close')
+    navMenu.classList.remove('nav-list-hide');
+    navMenu.classList.add('nav-list-show');
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    navMenu.classList.remove('nav-list-show');
+    navMenu.classList.add('nav-list-hide');
+    showMenu = false;
+  }
+}
+
+
+const navItems = document.querySelectorAll('.nav-item');
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    menuBtn.classList.remove('close');
+    navMenu.classList.remove('nav-list-show');
+    navMenu.classList.add('nav-list-hide');
+    showMenu = false;
+  })
+}
+);
