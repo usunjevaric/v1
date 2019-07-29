d79639e118f1ee482b8f1f4a6fbeadad78505d74
Sunjevaric <usunjevaric@gmail.com>
2019-07-29 12:57:12 +0200

    add scroll indicator on large screen

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ function changeNavigation() {
     document.getElementById('nav').classList.add('navigation');
     document.getElementById('nav').classList.remove('navigation-scrolled');
   }
+
 }
 
 //dropdown menu on small screens
@@ -58,3 +59,38 @@ work.forEach(item => {
 
   })
 })
+
+
+window.addEventListener('scroll', activeItem);
+function activeItem() {
+  const about = document.querySelector('#about-me').offsetTop;
+  const skills = document.querySelector('#skills').offsetTop;
+  const portfolio = document.querySelector('#portfolio').offsetTop;
+  const contact = document.querySelector('#contact').offsetTop;
+  const navItems = document.querySelectorAll('.nav-list li');
+  const scroll = window.pageYOffset;
+  if (scroll < about) {
+    deleteClass(navItems);
+    navItems[0].classList.add('nav-item-active')
+  } else if (scroll > about && scroll < skills) {
+    deleteClass(navItems);
+    navItems[1].classList.add('nav-item-active')
+  } else if (scroll > skills && scroll < portfolio) {
+    deleteClass(navItems);
+    navItems[2].classList.add('nav-item-active')
+  } else if (scroll > portfolio && scroll < contact) {
+    deleteClass(navItems);
+    navItems[3].classList.add('nav-item-active')
+  } else if (scroll > contact) {
+    deleteClass(navItems);
+    navItems[4].classList.add('nav-item-active')
+  }
+}
+
+function deleteClass(navItems) {
+  navItems.forEach(item => {
+    if (item.classList[1] !== undefined) {
+      item.classList.remove('nav-item-active');
+    }
+  })
+}
